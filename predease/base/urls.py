d51419a5de2451978bb76a890bed7f3bfcb11413
Mary Sandra <marysandrashibu@gmail.com>
2023-11-09 16:44:59 +0530

    predicteed list completed

--- predease/base/urls.py
+++ predease/base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="home"),
-    path('base/about.html',views.about, name='about'),
-    path('result/',views.result, name='result')
+    path('about/',views.about, name='about'),
+
 ]
--- predease/base/views.py
+++ predease/base/views.py
@@ -1,29 +1,37 @@
 from django.shortcuts import render,HttpResponse
+from sklearn import svm
 import pandas as pd
 import pickle
 from base.symptoms import List_of_symp
 # Create your views here.
 
 def home(request):
-    q=request.GET.get("q")
-    user_symp_list=q.split(",")
-    def convert(user_symp_lst):
-        inp_data=pd.DataFrame(0,index=range(1),columns=range(132))
-        for item in user_symp_lst:
-            if item in List_of_symp:
+    q = request.GET.get('q')
+
+    if q:
+        user_symp_list=q.split(",")
+
+        def convert(user_symp_lst):
+            inp_data=pd.DataFrame(0,index=range(1),columns=range(132))
+            for item in user_symp_lst:
+               
+               if item in List_of_symp:
+
                 t=List_of_symp.index(item)
                 inp_data[t]=1
-        return inp_data
-    
-    inp_to_model = convert(user_symp_list)
-
-    pickled_model = pickle.load(open('base\models\model.pkl', 'rb'))
-    res = pickled_model.predict(inp_to_model)
-    if q:
-        context = res
-        return render(request, "base/home.html", context)
+            return inp_data
+        inp_to_model = convert(user_symp_list)
+        pickled_model = pickle.load(open('base\models\model.pkl', 'rb'))
+        res = pickled_model.predict(inp_to_model)
+        
     else:
-        return render(request, "base/home.html")
+        res = ''
+
+    context= {'res':res}
+
+     
+    
+    return render(request, "base/home.html", context)
 
 
     
@@ -32,4 +40,24 @@ def about(request):
     return render(request,'base/about.html')
 
 def result(request):
+#q=request.GET.get("q")
+    #if q:
+       
+    # user_symp_list=q.split(",")
+    # def convert(user_symp_lst):
+    #     inp_data=pd.DataFrame(0,index=range(1),columns=range(132))
+    #     for item in user_symp_lst:
+    #         if item in List_of_symp:
+    #             t=List_of_symp.index(item)
+    #             inp_data[t]=1
+    #     return inp_data
+    
+    # inp_to_model = convert(user_symp_list)
+
+    # pickled_model = pickle.load(open('base\models\model.pkl', 'rb'))
+    # res = pickled_model.predict(inp_to_model)
+    # if q:
+    #     context={"res": res}
+    #     return render(request, "base/home.html", context)
+    # else:
     return HttpResponse("You seem to likely have .....")
